5f8566eff16a2616842897ed733408c7fa1e46c1
sneric <sneric.smith@gmail.com>
2019-05-17 16:30:44 -0600

    Initial commit

---
+++ ViewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder
+//
+//  Created by Eric Smith on 5/17/19.
+//  Copyright © 2019 Eric Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
